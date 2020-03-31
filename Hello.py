eb27b15ee9019f733c7f01d2e4b2550784304524
bholmes999 <bholmes@harding.edu>
2020-03-31 12:27:54 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,21 @@
+# Print Welcome Message
+#message = "Hello World"
+
+#print(message[:5])
+#print (message[6:])
+#print (message.upper())
+#print ('Number of times "Hello" apppears:')
+#print (message.count('Hello'))
+
+
+greeting = 'Hello'
+name = 'Brian'
+
+#message = '{}, {}. Welcome!'.format(greeting, name)
+message = f'{greeting}, {name}. Welcome!'
+
+print (message)
+
+#print(help(str))
+
+input('Press Enter to exit')
